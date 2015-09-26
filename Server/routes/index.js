a4978a3e01d8a64d0244ec9bd9976db321c26a82
Eunamong <ryuenuse@gmail.com>
2015-09-26 20:59:29 +0900

    create the skeleton of api

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//keyword를 받으면 가장 최근 분석 결과 제공
+//Query로 keyword받음
+router.get('/result/recently_result', function(req, res, next) {
+  res.sendStatus(200);
+});
+
+//keyword를 받으면 최근 분석 article 제공
+//Query로 keyword받음
+router.get('/result/article', function(req, res, next){
+  res.sendStatus(200);
+});
+
+//keyword를 받아 분석 시작
+//Query로 keyword받음
+router.post('/result/analysis', function(req, res, next){
+  res.sendStatus(200);
+});
+
 module.exports = router;
